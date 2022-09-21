8925d20010abe45681e8858e78a7c3efd3d0228f
juulrv <104025015+juulrv@users.noreply.github.com>
2022-09-21 10:34:39 +0200

    Nav werkt deels

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,13 @@
 // JavaScript Document
 console.log("hi");
+/****************************************/
+/* menu openen en sluiten met de button */
+/****************************************/
+var navButton = document.querySelector("nav button");
+
+navButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(){
+var deNav = document.querySelector("nav");
+deNav.classList.toggle("toonMenu");
+}
